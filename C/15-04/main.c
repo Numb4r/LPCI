a6479423f9043b754d37b89fa2a01c75641a4938
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-04-12 10:28:55 -0300

    Apagado o lixo do menu

--- C/15-04/main.c
+++ C/15-04/main.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include "4.h"
-#include "4.h"
+
 
 int main(int argc, char const *argv[]) {
-  /*int a = 2,b = 3;
-  troca(&a,&b);
-  printf("%d %d\n",a,b );*/
-
-//int a = 4,b = 2,c = 2;
-//printf("%f %f \n",media_9(a,b,c),desvio_padrao(a,b,c) );
-
-int a = 4, b = 2;
-troca(&a,&b);
-printf("%d %d\n",a,b );
-
-
+  
   return 0;
 }
