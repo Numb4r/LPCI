62d1dd5bce6f5cf45d47c62bef7a70902f27f738
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-04-26 10:56:26 -0300

    add 17-05

--- C/17-05/36.c
+++ C/17-05/36.c
@@ -4,8 +4,19 @@
 int main(int argc, char const *argv[]) {
   int op;
   float salario;
+  scanf("%d %f",&op,&salario );
   switch (op) {
-    case /* value */:
+    case 101:
+      printf("%f -> %f %f\n",salario,salario+salario*0.1,salario*0.1 );
+      break;
+    case 102:
+      printf("%f -> %f %f\n",salario,salario+salario*0.2,salario*0.2 );
+      break;
+    case 103:
+      printf("%f -> %f %f\n",salario,salario+salario*0.3,salario*0.3 );
+      break;
+    default:
+      printf("%f -> %f %f\n",salario,salario+salario*0.4,salario*0.4 );
   }
   return 0;
 }
