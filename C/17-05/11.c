71b3cfb2fb99c38f96aec1b95ab15685070085a4
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-05-03 08:38:40 -0300

    corrigido erro nas comparacoes

--- C/17-05/11.c
+++ C/17-05/11.c
@@ -4,15 +4,23 @@
 int main(int argc, char const *argv[]) {
   int a,b,c;
   scanf("%d %d %d",&a,&b,&c );
-  if (a < b) {
+  if (a < b && a < c) {
     if (b < c) {
       printf("%d %d %d\n",a,b,c );
     }else{
       printf("%d %d %d\n",a,c,b );
     }
-  }else{
-    if (b < c) {
+  }
+  if (b < c && b < a) {
+    if (a < c) {
+      printf("%d %d %d\n",b,a,c );
+    }else{
       printf("%d %d %d\n",b,c,a );
+    }
+  }
+  if (c < a && c < b) {
+    if (a < b ){
+      printf("%d %d %d\n",c,a,b );
     }else{
       printf("%d %d %d\n",c,b,a );
     }
