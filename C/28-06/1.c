c24611cc4746ce702deae797df1651e5b6673ad8
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-06-11 20:44:30 -0300

    Arrumado o numero de pessoas

--- C/28-06/1.c
+++ C/28-06/1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define QNT_PESSOAS 8
+//sao 8 pessoas
 int main(int argc, char const *argv[]) {
-  char nomes[8][30];
+  char nomes[QNT_PESSOAS][30];
   int quantidade[8];
   printf("\t\tBem vindo ao SLPFG\n" );//sistema da locadora para filmes gratis
   printf("\nEntre com o nome do cliente e a quantidade de filmes que ele alugou em 2010:\n" );
-  for (size_t i = 0; i < 8; i++) {
+  for (size_t i = 0; i < QNT_PESSOAS; i++) {
     printf("\nNome :");
     scanf("%s",nomes[i] );
     printf("\nQuantidade de filmes :");
@@ -15,7 +16,7 @@ int main(int argc, char const *argv[]) {
   }
 
   printf("\n\n\t\tTabela de filmes gratis\n\n" );
-  for (size_t i = 0; i < 8; i++) {
+  for (size_t i = 0; i < QNT_PESSOAS; i++) {
     printf("%s - > %d \n",nomes[i],quantidade[i]/10 );
   }
 
