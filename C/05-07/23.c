8e2b46ab7b3d40508720b5bc30ec80e3a9dba438
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-07-04 22:42:14 -0300

    i forgot to print(OOPS)

--- C/05-07/23.c
+++ C/05-07/23.c
@@ -10,6 +10,7 @@ int main(int argc, char const *argv[]) {
   Pessoa *p1 = (Pessoa*)malloc(sizeof(Pessoa));
   scanf("%s",p1->nome);
   scanf("%d",&p1->idade );
+  printf("%s %d\n",p1->nome,p1->idade );
   free(p1);
   return 0;
 }
